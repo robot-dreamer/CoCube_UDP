7d03b077d3fd99519f45140985ca9a49141da498
LiangShuai <sliang23@m.fudan.edu.cn>
2024-08-23 16:51:06 +0800

    测试

--- src/main.py
+++ src/main.py
@@ -4,13 +4,24 @@ import time
 import random
 
 if __name__ == '__main__':
-    robot1 = CoCube(1, enable_return=False)
+    robot1 = CoCube(3, enable_return=False)
+    robot2 = CoCube(4, enable_return=False)
+    robot3 = CoCube(5, enable_return=False)
+    robot4 = CoCube(6, enable_return=False)
     while True:
-        x = random.randint(1, 5)
-        y = random.randint(1, 5)
-        robot1.process_data(block="0", func="[display:mbPlot]", params=[x, y])
-        time.sleep(0.1)
-        robot1.process_data(block="0", func="[display:mbUnplot]", params=[x, y])
-        time.sleep(0.1)
+        for x in range(1, 6):
+            for y in range(1, 6):
+                robot1.process_data(block="0", func="[display:mbPlot]", params=[x, y])
+                robot2.process_data(block="0", func="[display:mbPlot]", params=[x, y])
+                robot3.process_data(block="0", func="[display:mbPlot]", params=[x, y])
+                robot4.process_data(block="0", func="[display:mbPlot]", params=[x, y])
+                time.sleep(0.1)
+        for x in range(1, 6):
+            for y in range(1, 6):
+                robot1.process_data(block="0", func="[display:mbUnplot]", params=[x, y])
+                robot2.process_data(block="0", func="[display:mbUnplot]", params=[x, y])
+                robot3.process_data(block="0", func="[display:mbUnplot]", params=[x, y])
+                robot4.process_data(block="0", func="[display:mbUnplot]", params=[x, y])
+                time.sleep(0.1)
 
 
